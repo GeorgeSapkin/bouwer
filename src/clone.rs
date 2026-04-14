41acfddd7c820908e5f5feff14bf8c7d72e4762a
George Sapkin <george@sapk.in>
2026-04-14 05:27:14 +0300

    refactor: clone macro
    Simplify clone macro and add support for specifying argument types.

--- src/clone.rs
+++ src/clone.rs
@@ -5,31 +5,19 @@
 // The reason for the strange syntax is for rustfm to see this as Rust code and
 // not a custom DSL, so it can format it.
 macro_rules! clone {
-    // clone!((a, b), move |x| { ... })
-    (($($n:ident),+), async move |$($arg:pat_param),+| $body:expr) => {
-        clone!(@gen ($($n),+) (async move) |$($arg),+| $body)
-    };
-    (($($n:ident),+), move |$($arg:pat_param),+| $body:expr) => {
-        clone!(@gen ($($n),+) (move) |$($arg),+| $body)
-    };
-
-    // clone!((a, b), move || { ... })
-    (($($n:ident),+), async move || $body:expr) => {
-        clone!(@gen ($($n),+) (async move) || $body)
-    };
-    (($($n:ident),+), move || $body:expr) => {
-        clone!(@gen ($($n),+) (move) || $body)
-    };
-
-    // clone!((a, b), async move { ... })
-    (($($n:ident),+), async move $body:block) => {
-        clone!(@gen ($($n),+) (async move) $body)
-    };
-
-    (@gen ($($n:ident),+) ($($prefix:tt)*) $($rest:tt)*) => {
+    // async move closure and block
+    (($($n:ident),+), async move $($rest:tt)*) => {
         {
             $( let $n = $n.clone(); )+
-            $($prefix)* $($rest)*
+            async move $($rest)*
+        }
+    };
+
+    // move closure
+    (($($n:ident),+), move $($rest:tt)*) => {
+        {
+            $( let $n = $n.clone(); )+
+            move $($rest)*
         }
     };
 }
